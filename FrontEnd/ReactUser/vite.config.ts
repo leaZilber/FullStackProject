be80458cacb3cd3b7a3a21b80ff788e7889d097b
leaZilber <lea417794@gmail.com>
2025-06-02 23:24:54 +0300

    13 errors render 4

--- FrontEnd/ReactUser/vite.config.ts
+++ FrontEnd/ReactUser/vite.config.ts
@@ -1,7 +1,22 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+//// https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//})
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react({
+        jsxImportSource: 'react'
+    })],
+    esbuild: {
+        jsx: 'automatic'
+    }
 })
