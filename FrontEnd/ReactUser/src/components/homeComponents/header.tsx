cff3ab8db12035c7f0284f30268480a0f61912af
leaZilber <lea417794@gmail.com>
2025-06-05 02:03:33 +0300

    46

--- FrontEnd/ReactUser/src/components/homeComponents/header.tsx
+++ FrontEnd/ReactUser/src/components/homeComponents/header.tsx
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
@@ -117,7 +116,6 @@ function HeaderPage() {
                         <Box sx={{ flexGrow: 0 }}>
                             <Tooltip title="Open settings">
                                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
                                 </IconButton>
                             </Tooltip>
                             <Menu
